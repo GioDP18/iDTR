3efb5c25e514ed5d4be8c30dcb39e2bb6d72c540
zhugeBot <zhugesomeone@gmail.com>
2024-02-06 23:27:32 +0800

    LoginPage

--- resources/js/Components/Views/LoginView.vue
+++ resources/js/Components/Views/LoginView.vue
@@ -1,13 +1,66 @@
 <script setup>
-    
+import { ref } from 'vue';
+
+const showPassword = ref(false);
+
+function togglePassword() {
+    showPassword.value = !showPassword.value;
+}
+
+function login() {
+    //Login function
+}
+
 </script>
 
 <template>
-    <div>
-        <h1>Login Page</h1>
+    <div class="main-container">
+        <div class="content">
+            <div class="logo-transparent">
+                <img src="../../../../public/storage/logo/logo-transparent.png" alt="">
+            </div>
+            <div class="text-center mt-4">
+                <h5 class="text-light">Unlocking productivity, one clock-in at a time. iDTR:
+                    Where dedication meets efficiency, shaping a seamless journey through time records
+                    and unlocking the door to professional growth.</h5>
+            </div>
+        </div>
+        <div class="login-container">
+            <div class="login-form">
+                <form class="form" @submit.prevent="login" method="POST">
+                    <p class="form-title">Sign in to your account</p>
+                    <div class="social-icons">
+                        <RouterLink to="/"><i class="fa-brands fa-google-plus-g"></i></RouterLink>
+                        <RouterLink to="/"><i class="fa-brands fa-facebook-f"></i></RouterLink>
+                    </div>
+                    <span>or use your username and password</span>
+                    <div class="input-container">
+                        <input type="text" name="username" placeholder="Enter Username" required>
+                        <span>
+                            <i class="fa-solid fa-user"></i>
+                        </span>
+                    </div>
+                    <div class="input-container">
+                        <input name="password" id="password" v-model="password" :type="showPassword ? 'text' : 'password'"
+                            placeholder="Enter Password" required>
+                        <span id="password" @click="togglePassword">
+                            <i :class="['fa-solid', showPassword ? 'fa-eye-slash' : 'fa-eye']"></i>
+                        </span>
+                    </div>
+                    <button class="submit" type="submit" name="login">
+                        Sign in
+                    </button>
+
+                    <p class="signup-link">
+                        No account?
+                        <RouterLink to="/register">Sign up</RouterLink>
+                    </p>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
-
+  
 <style scoped>
 
 </style>
--- resources/js/Route/index.js
+++ resources/js/Route/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import notFound from '../Components/404.vue';
 import LoginView from '../Components/Views/LoginView.vue';
+import RegisterView from '../Components/Views/RegisterView.vue';
 
 
 const routes = [
     {
         path : '/',
         component : LoginView,
+        
+    },
+    {
+        path : '/register',
+        component : RegisterView,
         
     },
 
