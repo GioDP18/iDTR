85e0fafba52d15bb6ab6ef9d8e235257539fc1e9
GioDP18 <giolagariza@gmail.com>
2024-02-08 14:21:44 +0800

    merged

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< Updated upstream
-=======
 
 Route::group([
     'middleware' => 'api',
@@ -31,4 +29,3 @@ Route::group([
     Route::get('/user-profile', [AuthController::class, 'userProfile']);    
     Route::post('/time-in-am', []);
 });
->>>>>>> Stashed changes
