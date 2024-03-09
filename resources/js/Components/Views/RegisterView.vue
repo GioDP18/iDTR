6fa895aa362227a6f7f3e81fae2545c83f58ce1f
Tristan Angelo <89288974+TNarido@users.noreply.github.com>
2024-03-10 01:51:29 +0800

    Update RegisterView.vue

--- resources/js/Components/Views/RegisterView.vue
+++ resources/js/Components/Views/RegisterView.vue
@@ -88,25 +88,25 @@ const handleRegister = async () => {
             password_confirmation: password_confirmation.value,
 
         })
-        .then((response) => {
-            if(response.data.success){
+            .then((response) => {
+                if (response.data.success) {
                     swal({
                         icon: "success",
                         text: response.data.message,
                     });
                     router.push('/')
                 }
-                else{
+                else {
                     swal({
                         icon: "error",
                         title: "Oops...",
                         text: response.data.message,
                     });
                 }
-        })
-        .finally(() => {
-            store.commit('setLoading', false)
-        })
+            })
+            .finally(() => {
+                store.commit('setLoading', false)
+            })
 
     } catch (error) {
         console.error('Error during registration:', error);
@@ -116,95 +116,121 @@ const handleRegister = async () => {
 <template>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-5 d-flex align-items-center justify-content-center"
-                style="background-color: white; height: 100vh;">
+            <div class="register-main-container col-sm-5 d-flex align-items-center justify-content-center">
                 <div class="register-container">
                     <div class="register-form">
                         <Form class="form" @submit="handleRegister" style="position:relative;">
                             <p class="form-title">Register your account</p>
                             <div class="row gap-2">
                                 <div class="col">
-                                    <div class="mt-4">
+                                    <div class="mt-4" style="text-align: start;">
+                                        <label for="firstname">First Name</label>
                                         <Field name="firstname" :rules="validateName" type="text" class="form-control"
-                                            placeholder="Fistname" v-model="firstname"/>
+                                            v-model="firstname" />
                                     </div>
                                     <ErrorMessage class="error-message" name="firstname" />
                                 </div>
                                 <div class="col">
-                                    <div class=" mt-4">
+                                    <div class="mt-4" style="text-align: start;">
+                                        <label for="middle">Middle Name</label>
                                         <Field name="middlename" :rules="validateName" type="text" class="form-control"
-                                            placeholder="Middlename" v-model="middlename"/>
+                                            v-model="middlename" />
                                     </div>
                                     <ErrorMessage class="error-message" name="middlename" />
                                 </div>
                             </div>
-                            <div class="input-group flex-nowrap mt-3">
-                                <Field name="lastname" :rules="validateName" type="text" class="form-control"
-                                    placeholder="Lastname" v-model="lastname"/>
+                            <div class="mt-3" style="text-align: start;">
+                                <label for="lastname">Last Name</label>
+                                <div class="input-group flex-nowrap ">
+                                    <Field name="lastname" :rules="validateName" type="text" class="form-control"
+                                        v-model="lastname" />
+                                </div>
                             </div>
                             <ErrorMessage class="error-message" name="lastname" />
-                            <div class="input-group flex-nowrap mt-3">
-                                <Field name="username" :rules="validateName" type="text" class="form-control"
-                                    placeholder="Username" v-model="username"/>
-                                <span class="input-group-text">
-                                    <i class="fa-solid fa-user"></i>
-                                </span>
+                            <div class="mt-3" style="text-align: start; display: flex; flex-direction: column;">
+                                <label for="username">Username</label>
+                                <div style="display: flex;" class="input-group flex-nowrap">
+                                    <Field name="username" :rules="validateName" type="text" class="form-control"
+                                        v-model="username" />
+                                    <span class="input-group-text">
+                                        <i class="fa-solid fa-user"></i>
+                                    </span>
+                                </div>
                             </div>
                             <ErrorMessage class="error-message" name="username" />
-                            <div class="mt-3 d-flex justify-content-around">
-                                <div class="form-check form-check-inline">
-                                    <Field class="form-check-input" :rules="validateName" type="radio" name="gender"
-                                        id="inlineRadio1" value="male" v-model="gender"/>
-                                    <label class="form-check-label" for="inlineRadio1" style="margin-left: 4px">Male</label>
-                                </div>
-                                <div class="form-check form-check-inline">
-                                    <Field class="form-check-input" :rules="validateName" type="radio" name="gender"
-                                        id="inlineRadio2" value="female" v-model="gender"/>
-                                    <label class="form-check-label" for="inlineRadio2"
-                                        style="margin-left: 4px; margin-right: 4px">Female</label>
+                            <div class="mt-3" style="text-align: start; display: flex; flex-direction: column;">
+                                <label for="gender">Gender</label>
+                                <div class="d-flex justify-content-evenly">
+                                    <div class=" form-check form-check-inline">
+                                        <Field class="form-check-input" :rules="validateName" type="radio" name="gender"
+                                            id="inlineRadio1" value="male" v-model="gender" />
+                                        <label class="form-check-label" for="inlineRadio1"
+                                            style="margin-left: 4px">Male</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <Field class="form-check-input" :rules="validateName" type="radio" name="gender"
+                                            id="inlineRadio2" value="female" v-model="gender" />
+                                        <label class="form-check-label" for="inlineRadio2"
+                                            style="margin-left: 4px; margin-right: 4px">Female</label>
+                                    </div>
                                 </div>
                             </div>
                             <ErrorMessage class="error-message" name="gender" />
-                            <div class="input-group flex-nowrap mt-3">
-                                <Field name="birthdate" :rules="validateName" type="date" class="form-control"
-                                    placeholder="" v-model="birthdate"/>
-                                <span class="input-group-text">
-                                    <i class="fa-solid fa-calendar"></i>
-                                </span>
+                            <div class="mt-3" style="text-align: start;">
+                                <label for="birthdate">Birthdate</label>
+                                <div class="input-group flex-nowrap ">
+                                    <Field name="birthdate" :rules="validateName" type="date" class="form-control"
+                                        placeholder="" v-model="birthdate" />
+                                    <span class="input-group-text">
+                                        <i class="fa-solid fa-calendar"></i>
+                                    </span>
+                                </div>
                             </div>
                             <ErrorMessage class="error-message" name="birthdate" />
-                            <div class="input-group flex-nowrap mt-3">
-                                <Field name="target_hours" :rules="validateName" type="text" class="form-control"
-                                    placeholder="HHH:MM:SS" v-model="target_hours"/>
-                                <span class="input-group-text">
-                                    <i class="fa-solid fa-calendar"></i>
-                                </span>
+                            <div class="mt-3" style="text-align: start;">
+                                <label for="birthdate">Target Hours</label>
+                                <div class="input-group flex-nowrap ">
+                                    <Field name="target_hours" :rules="validateName" type="text" class="form-control"
+                                        placeholder="HHH:MM:SS" v-model="target_hours" />
+                                    <span class="input-group-text">
+                                        <i class="fa-solid fa-calendar"></i>
+                                    </span>
+                                </div>
                             </div>
                             <ErrorMessage class="error-message" name="target_hours" />
-                            <div class="input-group flex-nowrap mt-3">
-                                <Field v-model="email" name="email" :rules="validateEmail" type="email" class="form-control"
-                                    placeholder="name@example.com"/>
-                                <span class="input-group-text">
-                                    <i class="fa-solid fa-envelope"></i>
-                                </span>
+                            <div class="mt-3" style="text-align: start;">
+                                <label for="birthdate">Email</label>
+                                <div class="input-group flex-nowrap">
+                                    <Field v-model="email" name="email" :rules="validateEmail" type="email"
+                                        class="form-control" />
+                                    <span class="input-group-text">
+                                        <i class="fa-solid fa-envelope"></i>
+                                    </span>
+                                </div>
                             </div>
                             <ErrorMessage class="error-message" name="email" />
-                            <div class="input-group flex-nowrap mt-3">
-                                <Field name="password" v-model="password" :rules="validatePassword"
-                                    :type="showPassword ? 'text' : 'password'" placeholder="Password"
-                                    class="form-control" />
-                                <span class="input-group-text" style="cursor:pointer;" @click="togglePassword">
-                                    <i :class="['fa-solid', showPassword ? 'fa-eye-slash' : 'fa-eye']"></i>
-                                </span>
+                            <div class="mt-3" style="text-align: start;">
+                                <label for="birthdate">Password</label>
+                                <div class="input-group flex-nowrap">
+                                    <Field name="password" v-model="password" :rules="validatePassword"
+                                        :type="showPassword ? 'text' : 'password'" class="form-control" />
+                                    <span class="input-group-text" style="cursor:pointer;" @click="togglePassword">
+                                        <i :class="['fa-solid', showPassword ? 'fa-eye-slash' : 'fa-eye']"></i>
+                                    </span>
+                                </div>
                             </div>
                             <ErrorMessage class="error-message" name="password" />
-                            <div class="input-group flex-nowrap mt-3">
-                                <Field name="password_confirmation" :rules="validateConfirmPassword"
-                                    :type="showConfirmPassword ? 'text' : 'password'" placeholder="Confirm Password"
-                                    class="form-control" v-model="password_confirmation"/>
-                                <span class="input-group-text" style="cursor:pointer;" @click="toggleConfirmPassword">
-                                    <i :class="['fa-solid', showConfirmPassword ? 'fa-eye-slash' : 'fa-eye']"></i>
-                                </span>
+                            <div class="mt-3" style="text-align: start;">
+                                <label for="birthdate">Confirm Password</label>
+                                <div class="input-group flex-nowrap ">
+                                    <Field name="password_confirmation" :rules="validateConfirmPassword"
+                                        :type="showConfirmPassword ? 'text' : 'password'" class="form-control"
+                                        v-model="password_confirmation" />
+                                    <span class="input-group-text" style="cursor:pointer;"
+                                        @click="toggleConfirmPassword">
+                                        <i :class="['fa-solid', showConfirmPassword ? 'fa-eye-slash' : 'fa-eye']"></i>
+                                    </span>
+                                </div>
                             </div>
                             <ErrorMessage class="error-message" name="password_confirmation" />
                             <button type="submit" class="submit mt-4">
@@ -219,11 +245,13 @@ const handleRegister = async () => {
                 </div>
             </div>
             <div class="col-sm-7 d-flex align-items-center justify-content-center"
-                style="background-color: #1269db; height: 100vh; padding:8em">
+                style="background-color: #1269db; padding:8em">
                 <div class="logo-container align-items-center text-center">
                     <img src="../../../../public/img/logo-transparent.png" alt="" style="width:12rem; height:10rem;">
-                    <h5 class="text-light mt-4">Unlocking productivity, one clock-in at a time. iDTR: Where dedication meets
-                        efficiency, shaping a seamless journey through time records and unlocking the door to professional
+                    <h5 class="text-light mt-4">Unlocking productivity, one clock-in at a time. iDTR: Where dedication
+                        meets
+                        efficiency, shaping a seamless journey through time records and unlocking the door to
+                        professional
                         growth.</h5>
                 </div>
             </div>
@@ -256,20 +284,25 @@ span i {
 
 input[type="date"]::-webkit-calendar-picker-indicator {
     display: none;
-  }
+}
 
-  /* For Firefox */
-  input[type="date"] {
+/* For Firefox */
+input[type="date"] {
     appearance: none;
     -moz-appearance: none;
     -webkit-appearance: none;
-  }
+}
 
 .error-message {
     color: red;
     font-size: 0.75rem;
 }
 
+.register-main-container {
+    background-color: white;
+    overflow-y: auto;
+    max-height: 100vh;
+}
 
 .register-container {
     display: flex;
@@ -279,11 +312,11 @@ input[type="date"]::-webkit-calendar-picker-indicator {
 
 .register-form {
     background-color: white;
-    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 1rem;
+    margin-top: 40vh;
 }
 
 .form {
@@ -293,6 +326,7 @@ input[type="date"]::-webkit-calendar-picker-indicator {
     max-width: 350px;
     border-radius: 0.5rem;
     box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+    height: 100%;
 }
 
 .form-title {
@@ -339,5 +373,17 @@ input[type="date"]::-webkit-calendar-picker-indicator {
 
 .signup-link a {
     text-decoration: underline;
+}
+
+@media screen and (max-width: 360px) {
+
+    .register-form {
+        margin-top: 0;
+    }
+
+    .register-main-container {
+        overflow: hidden;
+        max-height: 200vh;
+    }
 }
 </style>
